bbba8212dcba89eb3a0a8cf4044c48c1b765ad1e
Oswaldo <oswaldoe1992@gmail.com>
2024-05-21 21:17:26 -0400

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/OswaldoDM/charts-dashboard.git'  
+  base: 'https://oswaldodm.github.io/charts-dashboard/'  
  })
